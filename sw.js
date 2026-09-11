cd254bd2fbe5f2aeb2089ee867753e7239ef732e
joaubaron <joaubaron@users.noreply.github.com>
2026-09-11 19:08:27 +0000

    deploy: 1232c2b9e5c51e07781efb453ba392e2473e8d3a

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker
 // A versão é atualizada automaticamente pelo deploy.yml a cada push no GitHub Pages.
-const CACHE_VERSION = '11.09.2026-1603';
+const CACHE_VERSION = '11.09.2026-1608';
 const CACHE_NAME = `custo-pote-${CACHE_VERSION}`;
 
 const ASSETS = [
